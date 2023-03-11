b5d5c9fd09c80f010bb863c6379bb816932a994b
Enrriqu3 <125326292+Enrriqu3@users.noreply.github.com>
2023-03-10 19:45:18 -0600

    actividad

---
+++ Act.Semana7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Datos {
+    static void Main(string[] args) {
+        Console.Write("Ingrese un número entero: ");
+        int h = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j <= i; j++) {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
